61efd29a9b8dc4d04a2867958ac261ba482d531f
chairielazizi <azizihr2000@gmail.com>
2020-08-26 00:31:16 +0800

    configure the create exercise component

--- client/src/components/create-exercise.component.js
+++ client/src/components/create-exercise.component.js
@@ -1,6 +1,68 @@
 import React, { Component } from "react";
 
 export default class CreateExercises extends Component {
+  constructor(props) {
+    super(props); // always call super when defining constructor of a ssubclass
+
+    // point this to actual data
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+      description: "",
+      duration: 0,
+      date: new Date(),
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      users: ["test user"],
+      username: "test user",
+    });
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value, // just update the username
+    });
+  }
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value,
+    });
+  }
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value,
+    });
+  }
+  onChangeDate(date) {
+    this.setState({
+      date: date,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date,
+    };
+
+    console.log(exercise);
+
+    window.location = "/";
+  }
+
   render() {
     return (
       <div>
